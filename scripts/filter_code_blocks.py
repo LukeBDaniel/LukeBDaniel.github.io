10eeacd68c6a10a329e99add6300411ef44c87eb
Luke Daniel <92327776+LukeBDaniel@users.noreply.github.com>
2025-05-24 01:44:17 -0400

    Create filter_code_blocks.py

---
+++ scripts/filter_code_blocks.py
@@ -0,0 +1,29 @@
+import sys
+
+md_path = sys.argv[1]
+
+in_code = False
+keep = False
+output = []
+block = []
+
+with open(md_path) as f:
+    for line in f:
+        if line.startswith("```"):
+            if in_code and keep:
+                output.extend(block)
+                output.append(line)
+            elif not in_code:
+                block = [line]
+            in_code = not in_code
+            keep = False
+            continue
+        if in_code:
+            block.append(line)
+            if "# TAG: keep_code" in line:
+                keep = True
+        else:
+            output.append(line)
+
+with open(md_path, "w") as f:
+    f.writelines(output)
